dd056bb6a3d15d0e9da5c1d9c3b10a7b0673b322
Petru Flueras <petru.flueras@gmail.com>
2020-03-29 01:09:59 +0200

    Troubleshoot Travis config

--- release_deps.sh
+++ release_deps.sh
@@ -3,36 +3,36 @@
 VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 echo 'Releasing verion:' $VERSION
 
-git config --global user.email "petru.flueras@gmail.com"
-git config --global user.name "Petru Flueras"
+#git config --global user.email "petru.flueras@gmail.com"
+#git config --global user.name "Petru Flueras"
 
-cd $TRAVIS_BUILD_DIR/..
-git clone --depth=50 --branch=master https://${GITHUB_TOKEN}@github.com/pflueras/ci-module1.git
-cd ci-module1
+#cd $TRAVIS_BUILD_DIR/..
+#git clone --depth=50 --branch=master https://${GITHUB_TOKEN}@github.com/pflueras/ci-module1.git
+#cd ci-module1
 
 # Update project version and all org.examples dependencies
-mvn versions:set -DnewVersion=$VERSION
-mvn versions:use-dep-version -Dincludes=org.examples -DdepVersion=$VERSION -DforceVersion=true
+#mvn versions:set -DnewVersion=$VERSION
+#mvn versions:use-dep-version -Dincludes=org.examples -DdepVersion=$VERSION -DforceVersion=true
 
-git add pom.xml
-git commit -m "Release of version $VERSION"
-git tag "$VERSION" -m "Release version $VERSION"
+#git add pom.xml
+#git commit -m "Release of version $VERSION"
+#git tag "$VERSION" -m "Release version $VERSION"
 
 # New version
-MAJOR=$(echo $1 | cut -f 1 -d '.')
-MINOR=$(echo $1 | cut -f 2 -d '.')
-PATCH=$(echo $1 | cut -f 3 -d '.')
-NEW_VERSION=$MAJOR.$MINOR.$(($PATCH + 1))-SNAPSHOT
-echo 'New version:' $NEW_VERSION
+#MAJOR=$(echo $1 | cut -f 1 -d '.')
+#MINOR=$(echo $1 | cut -f 2 -d '.')
+#PATCH=$(echo $1 | cut -f 3 -d '.')
+#NEW_VERSION=$MAJOR.$MINOR.$(($PATCH + 1))-SNAPSHOT
+#echo 'New version:' $NEW_VERSION
 
 # Prepare for next development version
-mvn versions:set -DnewVersion=$NEW_VERSION
-mvn versions:use-dep-version -Dincludes=org.examples -DdepVersion=$NEW_VERSION -DforceVersion=true
-git add pom.xml
-git commit -m "Next development version $NEW_VERSION"
+#mvn versions:set -DnewVersion=$NEW_VERSION
+#mvn versions:use-dep-version -Dincludes=org.examples -DdepVersion=$NEW_VERSION -DforceVersion=true
+#git add pom.xml
+#git commit -m "Next development version $NEW_VERSION"
 
-git push --follow-tags
+#git push --follow-tags
 
 # Clean up
-cd $TRAVIS_BUILD_DIR
-rm -rf ci-module1
+#cd $TRAVIS_BUILD_DIR
+#rm -rf ci-module1
